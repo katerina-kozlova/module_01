fe713717b15bafb4c38ec0cef0eec09dfad0de0f
Екатерина Козлова <kate.rina.kozlova@yandex.ru>
2024-03-14 17:34:06 +0600

    do the second task

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-const amountOfGoods = document.querySelector(".cards__counter").innerHTML;
-const valueOfGoods = document.querySelector(".cards__price").innerHTML;
+'use strict';
 
-console.log(amountOfGoods * valueOfGoods);
+{
+    const nameOfGoods = "Cat litter";
+    const amountOfGoods = 52;
+    const valueOfGoods = 8.26;
+    const categoryOfGoods = "Care";
+
+    console.log(nameOfGoods);
+    console.log(`Общая сумма товара: ${amountOfGoods * valueOfGoods}`);
+}
+
+{
+    const nameOfGoods = "Whiskas";
+    const amountOfGoods = 352;
+    const valueOfGoods = 0.40;
+    const categoryOfGoods = "Pet Food";
+    
+    console.log(nameOfGoods);
+    console.log(`Общая сумма товара: ${amountOfGoods * valueOfGoods}`);
+}
