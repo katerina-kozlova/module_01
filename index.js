51e54543d3272916b68b3963913432b4631fa44e
Екатерина Козлова <kate.rina.kozlova@yandex.ru>
2024-03-14 16:39:18 +0600

    initial

---
+++ index.js
@@ -0,0 +1,4 @@
+const amountOfGoods = document.querySelector(".cards__counter").innerHTML;
+const valueOfGoods = document.querySelector(".cards__price").innerHTML;
+
+console.log(amountOfGoods * valueOfGoods);
