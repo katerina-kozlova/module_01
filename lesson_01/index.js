5af8db9a75c0b34cb898997a1b22f8384de7a4d9
Екатерина Козлова <kate.rina.kozlova@yandex.ru>
2024-03-18 17:50:07 +0600

    создать папки

---
+++ lesson_01/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const nameOfGoods = "Cat litter";
+const amountOfGoods = 52;
+const valueOfGoods = 8.26;
+
+console.log(nameOfGoods);
+console.log(amountOfGoods * valueOfGoods);
---
+++ lesson_02/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+{
+    const nameOfGoods = "Cat litter";
+    const amountOfGoods = 52;
+    const valueOfGoods = 8.26;
+    const categoryOfGoods = "Care";
+
+    console.log(nameOfGoods);
+    console.log(`Общая сумма товара: ${amountOfGoods * valueOfGoods}`);
+}
+
+{
+    const nameOfGoods = "Whiskas";
+    const amountOfGoods = 352;
+    const valueOfGoods = 0.40;
+    const categoryOfGoods = "Pet Food";
+    
+    console.log(nameOfGoods);
+    console.log(`Общая сумма товара: ${amountOfGoods * valueOfGoods}`);
+}
