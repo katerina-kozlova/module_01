b6ac4859537e70b9fa5ab6fa03f4e58fc750826d
Екатерина Козлова <kate.rina.kozlova@yandex.ru>
2024-03-26 14:15:21 +0600

    сделать тему 10

---
+++ lesson_10/cart.js
@@ -0,0 +1,55 @@
+'use strict';
+
+{
+    const cart = {
+        items: [],
+        count: 0,
+        // получить общую стоимость товаров
+        totalPrice() {
+            return this.calculateItemPrice();
+        },
+        // добавить товар
+        add(name, value, amount = 1) {
+            const newProduct = {
+                name: name,
+                value: value,
+                amount: amount,
+            }
+            // вызвать методы, чтобы свойства count и totalPrice были актуальны
+            this.items.push(newProduct);
+            this.increaseCount(amount);
+        },
+        // увеличить количество товаров
+        increaseCount(num) {
+            this.count += num;
+        },
+        // посчитать общую стоимость товаров
+        calculateItemPrice() {
+            // this.totalPrice = this.items.reduce(function (acc, item) {
+            //     acc + item.value * item.amount
+            // }, 0);
+            return this.items.reduce((acc, item) => acc + item.value * item.amount, 0);
+                                                // в каждой следующей итерации значение в acc будет результатом, 
+                                                // который вернулся на предыдущем шаге
+        },
+        // очистить корзину
+        clear() {
+            this.items = []; 
+            this.totalPrice = 0; 
+            this.count = 0; 
+        },
+        // вывести общую стомость корзины и распечатать
+        print() {
+            console.log(JSON.stringify(this.items));
+            console.log(`Общаяя стоимость товаров в корзине: ${this.totalPrice()}`);
+        },
+    }
+
+    cart.add("Cat litter", 8.26);
+    cart.add("Whiskas", 0.40, 3);
+    cart.add("Scratching post", 4.30, 1);
+
+    console.log(cart);
+
+    cart.print();
+}
