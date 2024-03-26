4dfd858cd40ad8b47b1e296ac03066f00a3e1993
Екатерина Козлова <kate.rina.kozlova@yandex.ru>
2024-03-26 11:25:46 +0600

    добавить комменты

--- lesson_09/cart.js
+++ lesson_09/cart.js
@@ -5,22 +5,27 @@
         items: [],
         totalPrice: 0,
         count: 0,
+        // получить общую стоимость товаров
         getTotalPrice() {
             return this.totalPrice;
         },
+        // добавить товар
         add(name, value, amount = 1) {
             const newProduct = {
                 name: name,
                 value: value,
                 amount: amount,
             }
+            // вызвать методы, чтобы свойства count и totalPrice были актуальны
             this.items.push(newProduct);
             this.increaseCount(amount);
             this.calculateItemPrice();
         },
+        // увеличить количество товаров
         increaseCount(num) {
             this.count += num;
         },
+        // посчитать общую стоимость товаров
         calculateItemPrice() {
             // this.totalPrice = this.items.reduce(function (acc, item) {
             //     acc + item.value * item.amount
@@ -29,11 +34,13 @@
                                                 // в каждой следующей итерации значение в acc будет результатом, 
                                                 // который вернулся на предыдущем шаге
         },
+        // очистить корзину
         clear() {
             this.items = []; 
             this.totalPrice = 0; 
             this.count = 0; 
         },
+        // вывести общую стомость корзины и распечатать
         print() {
             this.calculateItemPrice(); 
             console.log(JSON.stringify(this.items));
